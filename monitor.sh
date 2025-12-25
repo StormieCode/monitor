200c8a909818189d37eaa1186675f9fc4446807f
stormi <153160990+StormieCode@users.noreply.github.com>
2025-12-24 17:06:49 -0800

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+DELAY=$((RANDOM % 60 + 1))
+sleep "$DELAY"
+
+DATA=$(curl -s -L $LINK \
+  -H 'User-Agent: Mozilla/5.0' \
+  -H "Cookie: key=$COOKIE_KEY")
+
+send_alert () {
+  local webhook="$1"
+  local message="$2"
+
+  curl -s -X POST "$webhook" \
+    -H "Content-Type: application/json" \
+    -d "{\"content\": \"@everyone\\n$message\"}"
+}
+
+echo "$DATA" | jq -c '.[]' | while read -r game; do
+  plrs=$(echo "$game" | jq -r '.plrs')
+  max=$(echo "$game" | jq -r '.maxPlayers')
+  name=$(echo "$game" | jq -r '.Name')
+  creator=$(echo "$game" | jq -r '.creator // "Unknown"')
+  id=$(echo "$game" | jq -r '.id')
+  jobs=$(echo "$game" | jq -r '
+    if (.jobidsArray | length) > 0
+    then (.jobidsArray | map("• `" + . + "`") | join("\n"))
+    else "• None"
+    end
+  ')
+
+  # Skip if below lowest threshold
+  [[ "$plrs" -lt 200 ]] && continue
+
+  MESSAGE="🚨 **Player Threshold Hit**
+**$name**
+👥 Players: $plrs/$max
+👤 Creator: $creator
+🆔 Game ID: $id
+🧩 Job IDs:
+$jobs
+"
+
+  if [[ "$plrs" -ge 1000 ]]; then
+    send_alert "$DISCORD_WEBHOOK_1000" "$MESSAGE"
+
+  elif [[ "$plrs" -ge 300 ]]; then
+    send_alert "$DISCORD_WEBHOOK_300" "$MESSAGE"
+
+  elif [[ "$plrs" -ge 400 ]]; then
+    send_alert "$DISCORD_WEBHOOK_400" "$MESSAGE"
+
+  elif [[ "$plrs" -ge 200 ]]; then
+    send_alert "$DISCORD_WEBHOOK_200" "$MESSAGE"
+
+  elif [[ "$plrs" -ge 500 ]]; then
+    send_alert "$DISCORD_WEBHOOK_500" "$MESSAGE"
+  fi
+done
